1dcfeb20446e0bcc804a2f6516cb1f52bf51438d
Saqib Ayoub <saqibtelixy@gmail.com>
2024-09-25 16:48:17 +0530

    version 2.16.53

--- components/ui/HomeCard.tsx
+++ components/ui/HomeCard.tsx
@@ -4,12 +4,12 @@ interface HomeCardProps {
     img: string,
     title: string,
     description: string,
-    handleClick: () => void
+    handleClick: () => void,
 }
 
 const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
     return (
-        <div className={` ${className} px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer `} onClick={() => handleClick}>
+        <div className={` ${className} px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer `} onClick={handleClick} >
             <div className=" flex-center glassmorphism size-12 rounded-[10px] ">
                 <Image src={img} alt="meeting" width={27} height={27} />
             </div>
